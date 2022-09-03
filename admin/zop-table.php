bab9af2a6cf311560c6e9dc577b98e8ab433b67f
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-09-03 18:38:32 +0530

    operator status chage table and show status of table

--- admin/zop-table.php
+++ admin/zop-table.php
@@ -24,19 +24,118 @@ if (!isset($_SESSION['operator_id'])) {
             </div>
             <div style=" width: 90%;  height: 100%; position: absolute; margin-left: 10%;  ">
                 <?php
-                $sTables = "SELECT * FROM tables ";
-                $resTables = mysqli_query($conn, $sTables) or die("Query Faild sTables." . mysqli_connect_error());
 
-                while ($row = mysqli_fetch_assoc($resTables)) {  ?>
-                    <div class="container text-center">
-                        <p><?php echo "table id = " . $row['t_id']; ?> </p>
-                        <p><?php echo "table no or name = " . $row['t_name_or_num']; ?> </p>
-                        <hr>
-                    </div>
-                <?php } ?>
-                <?php
+                if (isset($_POST['table_status_active'])) {
+                    $Table_Active_update = "UPDATE `tables` SET `t_status`='1' WHERE `t_id` = {$_POST['table_id']} ";
+                    $result_update_active = mysqli_query($conn, $Table_Active_update) or die("Query Failed Table Active update problem.");
+                }
 
+                if (isset($_POST['table_status_disactive'])) {
+                    $Table_Disactive_update = "UPDATE `tables` SET `t_status`='0' WHERE `t_id` = {$_POST['table_id']} ";
+                    $result_update_disactive = mysqli_query($conn, $Table_Disactive_update) or die("Query Failed Table  Disactive update problem.");
+                }
+
+                $sTables = "SELECT * FROM `tables` ";
+
+                $Tables_show_all = mysqli_query($conn, $sTables) or die("Query Faild All table select." . mysqli_connect_error());
+
+                $Tables_select_disactive = "SELECT * FROM `tables` WHERE `t_status` = '0' ";
+
+                $Disactive_Tables = mysqli_query($conn, $Tables_select_disactive) or die("Query Faild sTables." . mysqli_connect_error());
+
+                $RunnigTableselect = "SELECT * FROM `tables` WHERE `t_status` = '1' ";
+
+                $RunnigTableShow = mysqli_query($conn, $RunnigTableselect) or die("Query Failed Runnig Table select.");
+                if (mysqli_num_rows($Tables_show_all) > 0) {
                 ?>
+                    <h1>Runnig Tables</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Tables</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Change Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            if (mysqli_num_rows($RunnigTableShow) == 0) {
+                                echo "</tbody>";
+                                echo "</table>";
+                                echo "<h3>There are currently no table running</h3>";
+                            } else {
+                                $column_no = 0;
+                                while ($rowRun = mysqli_fetch_assoc($RunnigTableShow)) { ?>
+                                    <tr>
+                                        <th scope="row"><?php echo ++$column_no; ?></th>
+                                        <td><?php echo $rowRun['t_name_or_num']; ?></td>
+                                        <td>
+                                            <p class="btn btn-info disabled">Runnig</p>
+                                        </td>
+                                        <td>
+                                            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                                                <div>
+                                                    <input type="hidden" name="table_id" value="<?php echo $rowRun['t_id']; ?>" required>
+                                                </div>
+                                                <input type="submit" name="table_status_disactive" class="btn btn-danger" value="Disactive">
+                                            </form>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                    <h1>Tables Available</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Tables</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Change Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php
+                            if (mysqli_num_rows($Disactive_Tables) == 0) {
+                                echo "</tbody>";
+                                echo "</table>";
+                                echo "<h3>all table is running</h3>";
+                            } else {
+                                $column_no_d = 0;
+                                while ($row = mysqli_fetch_assoc($Disactive_Tables)) {
+                            ?>
+                                    <tr>
+                                        <th scope="row"><?php echo ++$column_no_d; ?></th>
+                                        <td><?php echo $row['t_name_or_num']; ?></td>
+                                        <td>
+                                            <p class="btn btn-success disabled">Disactive</p>
+                                        </td>
+                                        <td>
+                                            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                                                <div>
+                                                    <input type="hidden" name="table_id" value="<?php echo $row['t_id']; ?>" required>
+                                                </div>
+                                                <input type="submit" name="table_status_active" class="btn btn-success" value="Active">
+                                            </form>
+                                        </td>
+                                    </tr>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                <?php
+                } else {
+                    echo "<h1> Tables Not avaliable (Because it is not given table from admin) </h1>";
+                }
+                ?>
+
             </div>
         </div>
 
