a09ff540c2c0e4c1ef914ce0a743b870bb264352
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2022-09-09 20:24:07 +0530

    admin panel add sidebar with icons

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -4,6 +4,16 @@ if (!isset($_SESSION['admin_id'])) {
     header("Location: {$homename}/index.php");
 } else {
 
+    $Admin_Name = "SELECT * FROM `admin_login` ";
+
+    $resultAdmin_Name = mysqli_query($conn, $Admin_Name) or die("Query Failed.");
+
+    if (mysqli_num_rows($resultAdmin_Name) > 0) {
+        while ($row = mysqli_fetch_assoc($resultAdmin_Name)) {
+            $AdminNameShow = $row['a_l_uname'];
+        }
+    }
+
 ?>
 
     <!DOCTYPE html>
@@ -15,14 +25,10 @@ if (!isset($_SESSION['admin_id'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&display=swap" rel="stylesheet">
-
         <!-- Icon Font Stylesheet -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="library/template/Icon/all.min.css" rel="stylesheet">
+        <link href="library/template/Icon/bootstrap-icons.css" rel="stylesheet">
+
 
         <!-- Libraries Stylesheet -->
         <link href="library/template/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
@@ -49,56 +55,56 @@ if (!isset($_SESSION['admin_id'])) {
             <!-- Sidebar Start -->
             <div class="sidebar pe-4 pb-3">
                 <nav class="navbar bg-light navbar-light">
-                    <a href="table.php" class="navbar-brand mx-4 mb-3">
-                        <h3 class="text-primary"></i>Admin</h3>
+                    <a href="table.php" class="navbar-brand mx-4 mb-3 ">
+                        <h3 class="text-primary ">Admin</h3>
                     </a>
                     <div class="d-flex align-items-center ms-4 mb-4">
                         <div class="position-relative">
-                            <img class="rounded-circle" src="library/template/img/person-circle.svg" alt="" style="width: 40px; height: 40px;">
+                            <img class="dash-icons m-2" src="library/icons/admin.gif" alt="" />
                             <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                         </div>
                         <div class="ms-3">
-                            <h6 class="mb-0">Jhon Doe</h6>
+                            <h6 class="mb-0 text-primary"><?php echo $AdminNameShow; ?></h6>
                         </div>
                     </div>
-                    <div class="navbar-nav w-100">
-                        <a href="table.php" class="nav-item nav-link active" title="Restaurant tables">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                    <div class="navbar-nav w-100 align-self-center">
+                        <a href="table.php" class="nav-item nav-link" title="Restaurant tables">
+                            <img class="dash-icons m-2" src="library/icons/table.png" alt="" />
                             Table
                         </a>
                         <a href="menu.php" class="nav-item nav-link " title="Food items">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/menu.png" alt="" />
                             Menu
                         </a>
                         <a href="category.php" class="nav-item nav-link " title="Food categories">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/category.png" alt="" />
                             Categories
                         </a>
                         <a href="admin-report.php" class="nav-item nav-link" title="Date wise profit">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/report.png" alt="" />
                             Report
                         </a>
                         <a href="feedback.php" class="nav-item nav-link " title="Customer feedback">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/feedback.png" alt="" />
                             Feedback
                         </a>
                         <a href="user.php" class="nav-item nav-link " title="Users information">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/users.png" alt="" />
                             Users
                         </a>
 
                         <div class="d-flex justify-content-center m-2">managements</div>
 
                         <a href="admin-time-manage.php" class="nav-item nav-link " title="Restaurant time management">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/time.png" alt="" />
                             Time
                         </a>
                         <a href="admin-management.php" class="nav-item nav-link " title="Restaurant managements">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/management.png" alt="" />
                             Managements
                         </a>
                         <a href="operator.php" class="nav-item nav-link " title="computer operators">
-                            <img class="rounded-circle dash-icons m-2" src="library/template/img/person-circle.svg" alt="" />
+                            <img class="dash-icons m-2" src="library/icons/operator.png" alt="" />
                             Operators
                         </a>
 
@@ -112,17 +118,16 @@ if (!isset($_SESSION['admin_id'])) {
             <div class="content">
                 <!-- Navbar Start -->
                 <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
-                    <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
-                        <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
+                    <a href="table.php" class="navbar-brand d-flex d-lg-none me-4">
+                        <img class="rounded-circle dash-icons m-2" src="library/icons/home.png" alt="" /></h2>
                     </a>
                     <a href="#" class="sidebar-toggler flex-shrink-0">
-                        <i class="fa fa-bars" style="color: #ffb30e !important;"></i>
+                        <img class="dash-icons m-2" src="library/icons/toggle-menu.png" alt="" />
                     </a>
                     <div class="navbar-nav align-items-center ms-auto">
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                                <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                                <span class="d-none d-lg-inline-flex">John Doe</span>
+                            <a href="#" class="nav-link" data-bs-toggle="dropdown">
+                                <img class="rounded-circle dash-icons m-2" src="library/icons/setting.png" alt="" />
                             </a>
                             <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                                 <a href="#" class="dropdown-item">My Profile</a>
